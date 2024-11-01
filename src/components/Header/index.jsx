9ab45094d84afc992859fe13cf0d87a0a32766f8
Nickolay <snsarkisyan@yandex.ru>
2024-11-01 22:57:01 +0300

    feat

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -11,6 +11,16 @@ const Header = () => {
     <div className="header">
       <Button onClick={onClose}>Закрыть</Button>
       <span className={"username"}>{tg.initDataUnsafe?.user?.username}</span>
+      <span className={"username"}>{tg.initDataUnsafe?.user?.first_name}</span>
+      <span className={"username"}>{tg.initDataUnsafe?.user?.last_name}</span>
+      <span className={"username"}>{tg.initDataUnsafe?.user?.id}</span>
+      <span className={"username"}>
+        {tg.initDataUnsafe?.user?.language_code}
+      </span>
+      <span className={"username"}>{tg.initDataUnsafe?.user?.is_premium}</span>
+      <span className={"username"}>
+        {window.Telegram.WebAppInitdataUnsafe?.user?.is_bot}
+      </span>
     </div>
   );
 };
